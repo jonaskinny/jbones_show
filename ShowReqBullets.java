3c1054c8d2911d6f9a28934036ad32d9f597af04
jonaskinny <jonaskinny@gmail.com>
2022-05-30 23:16:36 -0700

    Add files via upload

---
+++ ShowReqBullets.java
@@ -0,0 +1,53 @@
+package org.jbones.show;
+
+public class ShowReqBullets {
+    public static void Main(String[] args) {
+    // meaningless java class to set language in repository
+    // and register Copyright & Trademark & Reserved
+    System.out.println(System.lineSeparator() + "JBones Show Copyright 2016 Jonathan Cole JBones all rights reserved." + System.lineSeparator());
+    System.out.println("Read contents of this file [org.jbones.show.Main.java] for bulleted features list of JBones Show application");
+    
+    // Bulleted Features List as of 2016
+    // jcole
+    // jbones
+/*
+[layout]
+App
+- Role
+    : Module
+    : Module
+- Role
+    : Module
+    : Module
+    ...
+...
+
+[data]    
+Show (desktop)
+- ShowOp
+    : Notes Deck
+    : File Compare
+    : Content Modification (chainable tasks/replay/change order of exection)
+    : Workflow
+    : Notification
+    : Show Archive (.show file)
+- Producer
+    : People
+    : Roles
+    : Events
+    : Metrics/Analytics
+    : Role Alias (Producer can execute ShowOp/Coordinator/Speaker actions)
+- Coordinator
+    : Versioning
+    : Merging
+- Speaker
+    : Thin Client (web/mob-web)
+    : Upload
+    : Download
+    : Read-Only Event View
+    
+    */
+}
+}
+
+
