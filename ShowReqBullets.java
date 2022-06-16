3b363909ea17c7c1d5862574e0c40bd3f0f3616c
jonaskinny <jonaskinny@gmail.com>
2022-06-15 19:00:59 -0700

    Add files via upload

--- ShowReqBullets.java
+++ ShowReqBullets.java
@@ -2,7 +2,7 @@ package org.jbones.show;
 
 public class ShowReqBullets {
     public static void Main(String[] args) {
-    // meaningless java class to set language in repository
+    // intended to set language in repository
     // and register Copyright & Trademark & Reserved
     System.out.println(System.lineSeparator() + "JBones Show Copyright 2016 Jonathan Cole JBones all rights reserved." + System.lineSeparator());
     System.out.println("Read contents of this file [org.jbones.show.Main.java] for bulleted features list of JBones Show application");
@@ -45,6 +45,30 @@ Show (desktop)
     : Upload
     : Download
     : Read-Only Event View
+    
+    ...
+    ...
+    ...
+    
+    
+    modules for ...
+    
+    NotesDeck generation
+    Workflow (aka function stack, orderable treatments to files, etc)
+    Approvals
+    Document repository
+    Archiving entire contents (.show file)
+    Analytics
+    Version Control
+    Visual Diff/fast file triage
+    File comparison from all inbound channels to detect updates on outdated file versions
+    File locking once file(artifact) enters network/single desktop install - regardless of file
+    location.
+    Dynamic aggressive network on local wan/lan/etc. (collapse from wide team down to ShowOp as defacto repository
+    so team can collaborate during chaotic production and default collapse repository into smaller networked groups
+    until only ShowOp is left. This would be the defacto show file in crisis
+    
+    
     
     */
 }
